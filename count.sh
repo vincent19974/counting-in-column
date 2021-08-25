8781bf4153381ae6eaccdcdcf78767aec2e50be1
vincent19974 <nambatac.vince@gmail.com>
2021-08-25 13:52:40 +0800

    shell script for counting values in columns

--- count.sh
+++ count.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+sudo lvs --separator '.' | awk -F '.' '{printf "%10s \n", $4}' > lsize.tmp && awk '{if (NR>2) {print}}' lsize.tmp > lsize.txt && rm -r lsize.tmp
+awk -F, '{sum=0; for (i=1; i<=NF; i++) {sum+= $i} print sum}' lsize.txt > output.txt
+
+awk -F',' '{ sum += $1 }
+           END{ printf "Total LSize: %dGB \n", sum,a NR }' output.txt > total
+rm -r lsize.txt
+rm -r output.txt
 
